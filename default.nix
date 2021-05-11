2d65a8566c6bb24c8b7b82c45439b42ebdaec697
John Wiegley <johnw@newartisans.com>
2021-05-11 12:26:32 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,26 +1,23 @@
-{ rev    ? "28c2c0156da98dbe553490f904da92ed436df134"
-, sha256 ? "04f3qqjs5kd5pjmqxrngjrr72lly5azcr7njx71nv1942yq1vy2f"
-, pkgs   ?
-  import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256; }) {
-    config.allowUnfree = true;
-    config.allowBroken = true;
-    config.packageOverrides = pkgs: rec {
-      sitebuilder = pkgs.callPackage ~/src/sitebuilder {
-        compiler = "ghc883";
-      };
-    };
-  }
+{ pkgs ? (import <darwin> {}).pkgs
+
+# , rev    ? "28c2c0156da98dbe553490f904da92ed436df134"
+# , sha256 ? "04f3qqjs5kd5pjmqxrngjrr72lly5azcr7njx71nv1942yq1vy2f"
+# , pkgs   ? import (builtins.fetchTarball {
+#     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+#     inherit sha256; }) {
+#     config.allowUnfree = true;
+#   }
 
 , mkDerivation ? null
+, yuicompressor  ? pkgs.yuicompressor
+, sitebuilder ? pkgs.callPackage ~/src/sitebuilder { inherit pkgs yuicompressor; }
 }:
 
 with pkgs; stdenv.mkDerivation {
   name = "johnwiegley";
   src = ./.;
 
-  buildInputs = [ yuicompressor pkgs.sitebuilder ];
+  buildInputs = [ yuicompressor sitebuilder ];
 
   buildPhase = "sitebuilder rebuild";
 
